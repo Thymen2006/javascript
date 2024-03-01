c0c44020d04797a5853b61b102866a76066c1c68
thymen2006 <2185395@talnet.nl>
2024-03-01 13:15:08 +0100

    javascript eindopdracht
    heb er verder aan gewerk en hoop dast het af is

--- javascript yahtzee eindopdracht/yahtzee.js
+++ javascript yahtzee eindopdracht/yahtzee.js
@@ -1,6 +1,6 @@
 const canThrow = [true,true,true,true,true];
-const vast1 = [true,true,true,true,true,true];
-const vast2 = [true,true,true,true,true,true];
+const vast1 = [true,true,true,true,true,true,true,true,true,true,true,true,true];
+const vast2 = [true,true,true,true,true,true,true,true,true,true,true,true,true];
 
 let keer = 0;
 
@@ -13,16 +13,8 @@ let ogen4 = 1;
 const dice = [0,0,0,0,0,0];
 
 const dices = [0,0,0,0,0,0];
-// let dice1;
-// let dice2;
-// let dice3;
-// let dice4;
-// let dice5;
-// let dice6;
-// let subtotaal = dice1 + dice2 + dice3 + dice4 + dice5 + dice6;
-// let bonus = 0;
-// if(subtotaal >= 60){bonus = 35;}
-// let totaal = subtotaal + bonus;
+const dices1 = [0,0,0,0,0,0,0];
+const dices2 = [0,0,0,0,0,0,0];
 
 function dobel(){
     const gerolt = [];
@@ -42,18 +34,6 @@ function dobel(){
 
     gerolt.push(ogen, ogen1, ogen2, ogen3, ogen4);
     console.log(gerolt);
-
-    // function countNumber(gerolt, value) {
-    //     var count = 0;
-    //     gerolt.forEach((v) => (v === value && count++));
-    //     return count;
-    // }
-    // console.log("1 = " + countNumber(gerolt, 1));
-    // console.log("2 = " + countNumber(gerolt, 2));
-    // console.log("3 = " + countNumber(gerolt, 3));
-    // console.log("4 = " + countNumber(gerolt, 4));
-    // console.log("5 = " + countNumber(gerolt, 5));
-    // console.log("6 = " + countNumber(gerolt, 6));
 
     if(gerolt[0] === 1){document.getElementById("img1").src = "foto/1ogen.png";}
     if(gerolt[1] === 1){document.getElementById("img2").src = "foto/1ogen.png";}
@@ -113,12 +93,6 @@ function dobel(){
     dices[4] = dice[4] * 5;
     dices[5] = dice[5] * 6;
 
-    // const dice1 = dice[0] * 1;
-    // const dice2 = dice[1] * 2;
-    // const dice3 = dice[2] * 3;
-    // const dice4 = dice[3] * 4;
-    // const dice5 = dice[4] * 5;
-    // const dice6 = dice[5] * 6;
     const subtotaal1 = subcount1[0] + subcount1[1] + subcount1[2] + subcount1[3] + subcount1[4] + subcount1[5];
     const subtotaal2 = subcount2[0] + subcount2[1] + subcount2[2] + subcount2[3] + subcount2[4] + subcount2[5];
     let bonus1 = 0;
@@ -127,56 +101,230 @@ function dobel(){
     if(subtotaal2 >= 60){bonus2 = 35;}
     const totaal1 = subtotaal1 + bonus1;
     const totaal2 = subtotaal2 + bonus2;
+
+    let three1 = 0;
+    let three2 = 0;
+    if(dice[0] == 3||dice[0] == 4||dice[0] == 5){three1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[0] == 3||dice[0] == 4||dice[0] == 5){three2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[1] == 3||dice[1] == 4||dice[1] == 5){three1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[1] == 3||dice[1] == 4||dice[1] == 5){three2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[2] == 3||dice[2] == 4||dice[2] == 5){three1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[2] == 3||dice[2] == 4||dice[2] == 5){three2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[3] == 3||dice[3] == 4||dice[3] == 5){three1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[3] == 3||dice[3] == 4||dice[3] == 5){three2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[4] == 3||dice[4] == 4||dice[4] == 5){three1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[4] == 3||dice[4] == 4||dice[4] == 5){three2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[5] == 3||dice[5] == 4||dice[5] == 5){three1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[5] == 3||dice[5] == 4||dice[5] == 5){three2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+
+    let four1 = 0;
+    let four2 = 0;
+    if(dice[0] == 4||dice[0] == 5){four1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[0] == 4||dice[0] == 5){four2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[1] == 4||dice[1] == 5){four1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[1] == 4||dice[1] == 5){four2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[2] == 4||dice[2] == 5){four1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[2] == 4||dice[2] == 5){four2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[3] == 4||dice[3] == 5){four1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[3] == 4||dice[3] == 5){four2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[4] == 4||dice[4] == 5){four1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[4] == 4||dice[4] == 5){four2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[5] == 4||dice[5] == 5){four1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+    if(dice[5] == 4||dice[5] == 5){four2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5]}
+
+    let full1 = 0;
+    let full2 = 0;
+    if(dice[0] == 3 && dice[1] == 2 ||dice[0] == 3 && dice[2] == 2||dice[0] == 3 && dice[3] == 2||dice[0] == 3 && dice[4] == 2||dice[0] == 3 && dice[5] == 2){full1 = 25}
+    if(dice[0] == 3 && dice[1] == 2 ||dice[0] == 3 && dice[2] == 2||dice[0] == 3 && dice[3] == 2||dice[0] == 3 && dice[4] == 2||dice[0] == 3 && dice[5] == 2){full2 = 25}
+    if(dice[1] == 3 && dice[0] == 2 ||dice[1] == 3 && dice[2] == 2||dice[1] == 3 && dice[3] == 2||dice[1] == 3 && dice[4] == 2||dice[1] == 3 && dice[5] == 2){full1 = 25}
+    if(dice[1] == 3 && dice[0] == 2 ||dice[1] == 3 && dice[2] == 2||dice[1] == 3 && dice[3] == 2||dice[1] == 3 && dice[4] == 2||dice[1] == 3 && dice[5] == 2){full2 = 25}
+    if(dice[2] == 3 && dice[1] == 2 ||dice[2] == 3 && dice[0] == 2||dice[2] == 3 && dice[3] == 2||dice[2] == 3 && dice[4] == 2||dice[2] == 3 && dice[5] == 2){full1 = 25}
+    if(dice[2] == 3 && dice[1] == 2 ||dice[2] == 3 && dice[0] == 2||dice[2] == 3 && dice[3] == 2||dice[2] == 3 && dice[4] == 2||dice[2] == 3 && dice[5] == 2){full2 = 25}
+    if(dice[3] == 3 && dice[1] == 2 ||dice[3] == 3 && dice[2] == 2||dice[3] == 3 && dice[0] == 2||dice[3] == 3 && dice[4] == 2||dice[3] == 3 && dice[5] == 2){full1 = 25}
+    if(dice[3] == 3 && dice[1] == 2 ||dice[3] == 3 && dice[2] == 2||dice[3] == 3 && dice[0] == 2||dice[3] == 3 && dice[4] == 2||dice[3] == 3 && dice[5] == 2){full2 = 25}
+    if(dice[4] == 3 && dice[1] == 2 ||dice[4] == 3 && dice[2] == 2||dice[4] == 3 && dice[3] == 2||dice[4] == 3 && dice[0] == 2||dice[4] == 3 && dice[5] == 2){full1 = 25}
+    if(dice[4] == 3 && dice[1] == 2 ||dice[4] == 3 && dice[2] == 2||dice[4] == 3 && dice[3] == 2||dice[4] == 3 && dice[0] == 2||dice[4] == 3 && dice[5] == 2){full2 = 25}
+    if(dice[5] == 3 && dice[1] == 2 ||dice[5] == 3 && dice[2] == 2||dice[5] == 3 && dice[3] == 2||dice[5] == 3 && dice[4] == 2||dice[5] == 3 && dice[1] == 2){full1 = 25}
+    if(dice[5] == 3 && dice[1] == 2 ||dice[5] == 3 && dice[2] == 2||dice[5] == 3 && dice[3] == 2||dice[5] == 3 && dice[4] == 2||dice[5] == 3 && dice[1] == 2){full2 = 25}
+
+    let klein1 = 0;
+    let klein2 = 0;
+    if(dice[0] && dice[1] && dice[2] && dice[3] || dice[2] && dice[3] && dice[4] && dice[5] || dice[1] && dice[2] && dice[3] && dice[4]){klein1 = 30}
+    if(dice[0] && dice[1] && dice[2] && dice[3] || dice[2] && dice[3] && dice[4] && dice[5] || dice[1] && dice[2] && dice[3] && dice[4]){klein2 = 30}
+
+    let groot1 = 0;
+    let groot2 = 0;
+    if(dice[0] && dice[1] && dice[2] && dice[3] && dice[4] || dice[1] && dice[2] && dice[3] && dice[4] && dice[5]){groot1 = 40}
+    if(dice[0] && dice[1] && dice[2] && dice[3] && dice[4] || dice[1] && dice[2] && dice[3] && dice[4] && dice[5]){groot2 = 40}
+
+    let yathz1 = 0;
+    let yathz2 = 0;
+    if(dice[0] == 5){yathz1 = 50}if(dice[1] == 5){yathz1 = 50}if(dice[2] == 5){yathz1 = 50}if(dice[3] == 5){yathz1 = 50}if(dice[4] == 5){yathz1 = 50}if(dice[5] == 5){yathz1 = 50}
+    if(dice[0] == 5){yathz2 = 50}if(dice[1] == 5){yathz2 = 50}if(dice[2] == 5){yathz2 = 50}if(dice[3] == 5){yathz2 = 50}if(dice[4] == 5){yathz2 = 50}if(dice[5] == 5){yathz2 = 50}
+
+    let chance1 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5];
+    let chance2 = dices[0] + dices[1] + dices[2] + dices[3] + dices[4] + dices[5];
+
+    let bereken1 = count1[0] + count1[1] + count1[2] + count1[3] + count1[4] + count1[5] + count1[6];
+    let bereken2 = count2[0] + count2[1] + count2[2] + count2[3] + count2[4] + count2[5] + count2[6];
+    let totaalal1 = bereken1 + totaal1;
+    let totaalal2 = bereken2 + totaal2;
+
+    dices1[0] = three1;
+    dices1[1] = four1;
+    dices1[2] = full1;
+    dices1[3] = klein1;
+    dices1[4] = groot1;
+    dices1[5] = yathz1;
+    dices1[6] = chance1;
+
+    dices2[0] = three2;
+    dices2[1] = four2;
+    dices2[2] = full2;
+    dices2[3] = klein2;
+    dices2[4] = groot2;
+    dices2[5] = yathz2;
+    dices2[6] = chance2;
     
     if(vast1[0] === true){document.getElementById("een").innerHTML = "een = " + dices[0];
-    }else{document.getElementById("eenb").disabled = true;}
+    }else{document.getElementById("eena").disabled = true;}
     if(vast1[1] === true){document.getElementById("twee").innerHTML = "twee = " + dices[1];
-    }else{document.getElementById("tweeb").disabled = true;}
+    }else{document.getElementById("tweea").disabled = true;}
     if(vast1[2] === true){document.getElementById("drie").innerHTML = "drie = " + dices[2];
-    }else{document.getElementById("drieb").disabled = true;}
+    }else{document.getElementById("driea").disabled = true;}
     if(vast1[3] === true){document.getElementById("vier").innerHTML = "vier = " + dices[3];
-    }else{document.getElementById("vierb").disabled = true;}
+    }else{document.getElementById("viera").disabled = true;}
     if(vast1[4] === true){document.getElementById("vijf").innerHTML = "vijf = " + dices[4];
-    }else{document.getElementById("vijfb").disabled = true;}
+    }else{document.getElementById("vijfa").disabled = true;}
     if(vast1[5] === true){document.getElementById("zes").innerHTML = "zes = " + dices[5];
-    }else{document.getElementById("zesb").disabled = true;}
-
+    }else{document.getElementById("zesa").disabled = true;}
     document.getElementById("subtotaal1").innerHTML = "subtotaal = " + subtotaal1;
     document.getElementById("bonus1").innerHTML = "bonus = " + bonus1;
     document.getElementById("totaal1").innerHTML = "totaal = " + totaal1;
+    if(vast1[6] === true){document.getElementById("three1").innerHTML = "three of a kind = " + dices1[0];
+    }else{document.getElementById("threea").disabled = true;}
+    if(vast1[7] === true){document.getElementById("four1").innerHTML = "four of a kind = " + dices1[1];
+    }else{document.getElementById("foura").disabled = true;}
+    if(vast1[8] === true){document.getElementById("full1").innerHTML = "(25)full house = " + dices1[2];
+    }else{document.getElementById("fulla").disabled = true;}
+    if(vast1[9] === true){document.getElementById("klein1").innerHTML = "(30)kleine straat = " + dices1[3];
+    }else{document.getElementById("kleina").disabled = true;}
+    if(vast1[10] === true){document.getElementById("groot1").innerHTML = "(40)groote straat = " + dices1[4];
+    }else{document.getElementById("groota").disabled = true;}
+    if(vast1[11] === true){document.getElementById("yahtzee1").innerHTML = "(50)yahtzee = " + dices1[5];
+    }else{document.getElementById("yathzeea").disabled = true;}
+    if(vast1[12] === true){document.getElementById("chance1").innerHTML = "chance = " + dices1[6];
+    }else{document.getElementById("chancea").disabled = true;}
+    document.getElementById("totaalall1").innerHTML = "totaal alles = " + totaalal1;
 
 
 
-    if(vast1[0] === true){document.getElementById("een2").innerHTML = "een = " + dices[0];
-    }else{document.getElementById("eenb2").disabled = true;}
-    if(vast1[1] === true){document.getElementById("twee2").innerHTML = "twee = " + dices[1];
-    }else{document.getElementById("tweeb2").disabled = true;}
-    if(vast1[2] === true){document.getElementById("drie2").innerHTML = "drie = " + dices[2];
-    }else{document.getElementById("drieb2").disabled = true;}
-    if(vast1[3] === true){document.getElementById("vier2").innerHTML = "vier = " + dices[3];
-    }else{document.getElementById("vierb2").disabled = true;}
-    if(vast1[4] === true){document.getElementById("vijf2").innerHTML = "vijf = " + dices[4];
-    }else{document.getElementById("vijfb2").disabled = true;}
-    if(vast1[5] === true){document.getElementById("zes2").innerHTML = "zes = " + dices[5];
-    }else{document.getElementById("zesb2").disabled = true;}
-
+    if(vast2[0] === true){document.getElementById("een2").innerHTML = "een = " + dices[0];
+    }else{document.getElementById("eenb").disabled = true;}
+    if(vast2[1] === true){document.getElementById("twee2").innerHTML = "twee = " + dices[1];
+    }else{document.getElementById("tweeb").disabled = true;}
+    if(vast2[2] === true){document.getElementById("drie2").innerHTML = "drie = " + dices[2];
+    }else{document.getElementById("drieb").disabled = true;}
+    if(vast2[3] === true){document.getElementById("vier2").innerHTML = "vier = " + dices[3];
+    }else{document.getElementById("vierb").disabled = true;}
+    if(vast2[4] === true){document.getElementById("vijf2").innerHTML = "vijf = " + dices[4];
+    }else{document.getElementById("vijfb").disabled = true;}
+    if(vast2[5] === true){document.getElementById("zes2").innerHTML = "zes = " + dices[5];
+    }else{document.getElementById("zesb").disabled = true;}
     document.getElementById("subtotaal2").innerHTML = "subtotaal = " + subtotaal2;
     document.getElementById("bonus2").innerHTML = "bonus = " + bonus2;
     document.getElementById("totaal2").innerHTML = "totaal = " + totaal2;
+    if(vast2[6] === true){document.getElementById("three2").innerHTML = "three of a kind = " + dices2[0];
+    }else{document.getElementById("threeb").disabled = true;}
+    if(vast2[7] === true){document.getElementById("four2").innerHTML = "four of a kind = " + dices2[1];
+    }else{document.getElementById("fourb").disabled = true;}
+    if(vast2[8] === true){document.getElementById("full2").innerHTML = "(25)full house = " + dices2[2];
+    }else{document.getElementById("fullb").disabled = true;}
+    if(vast2[9] === true){document.getElementById("klein2").innerHTML = "(30)kleine straat = " + dices2[3];
+    }else{document.getElementById("kleinb").disabled = true;}
+    if(vast2[10] === true){document.getElementById("groot2").innerHTML = "(40)groote straat = " + dices2[4];
+    }else{document.getElementById("grootb").disabled = true;}
+    if(vast2[11] === true){document.getElementById("yahtzee2").innerHTML = "(50)yahtzee = " + dices2[5];
+    }else{document.getElementById("yathzeeb").disabled = true;}
+    if(vast2[12] === true){document.getElementById("chance2").innerHTML = "chance = " + dices2[6];
+    }else{document.getElementById("chanceb").disabled = true;}
+    document.getElementById("totaalall2").innerHTML = "totaal alles = " + totaalal2;
 }
 
 const subcount1 = [];
+const count1 = [];
+console.log(count1);
 
 function vasta(welke){
     vast1[welke - 1] = false;
-    subcount1.push(dices[welke - 1])
+    subcount1.push(dices[welke - 1]);
+}
+function vasta1(){
+    vast1[6] = false;
+    count1.push(dices1[0]);
+}
+function vasta2(){
+    vast1[7] = false;
+    count1.push(dices1[1]);
+}
+function vasta3(){
+    vast1[8] = false;
+    count1.push(dices1[2]);
+}
+function vasta4(){
+    vast1[9] = false;
+    count1.push(dices1[3]);
+}
+function vasta5(){
+    vast1[10] = false;
+    count1.push(dices1[4]);
+}
+function vasta6(){
+    vast1[11] = false;
+    count1.push(dices1[5]);
+}
+function vasta7(){
+    vast1[12] = false;
+    count1.push(dices1[6]);
 }
 
+
+
 const subcount2 = [];
+const count2 = [];
+console.log(count2);
 
 function vastb(welke){
     vast2[welke - 1] = false;
     subcount2.push(dices[welke - 1])
+}
+function vastb1(){
+    vast2[6] = false;
+    count2.push(dices2[0]);
+}
+function vastb2(){
+    vast2[7] = false;
+    count2.push(dices2[1]);
+}
+function vastb3(){
+    vast2[8] = false;
+    count2.push(dices2[2]);
+}
+function vastb4(){
+    vast2[9] = false;
+    count2.push(dices2[3]);
+}
+function vastb5(){
+    vast2[10] = false;
+    count2.push(dices2[4]);
+}
+function vastb6(){
+    vast2[11] = false;
+    count2.push(dices2[5]);
+}
+function vastb7(){
+    vast2[12] = false;
+    count2.push(dices2[6]);
 }
 
 function onthoud(welke){
@@ -186,6 +334,10 @@ function onthoud(welke){
     document.getElementById("onthoud" + welke).classList.toggle("is-active"); 
 }
 function volgende(){
+    document.getElementById("P1").classList.toggle("is-active");
+    document.getElementById("P2").classList.toggle("is-active");
+    document.getElementById("B1").classList.toggle("is-active");
+    document.getElementById("B2").classList.toggle("is-active");
     document.getElementById("gooien").disabled = false;
     document.getElementById("onthoud1").classList.remove("is-active");
     document.getElementById("onthoud2").classList.remove("is-active");
@@ -197,35 +349,19 @@ function volgende(){
     canThrow[2] = true;
     canThrow[3] = true;
     canThrow[4] = true;
+    keer = 0;
 }
-
-// function onthoud1(){
-//     if(canThrow[0] == false){
-//         canThrow[0] = true;
-//     }else{canThrow[0] = false;}
-//     document.getElementById("onthoud1").classList.toggle("is-active"); 
-// }
-// function onthoud2(){
-//     if(canThrow[1] == false){
-//         canThrow[1] = true;
-//     }else{canThrow[1] = false;}
-//     document.getElementById("onthoud2").classList.toggle("is-active"); 
-// }
-// function onthoud3(){
-//     if(canThrow[2] == false){
-//         canThrow[2] = true;
-//     }else{canThrow[2] = false;}
-//     document.getElementById("onthoud3").classList.toggle("is-active"); 
-// }
-// function onthoud4(){
-//     if(canThrow[3] == false){
-//         canThrow[3] = true;
-//     }else{canThrow[3] = false;}
-//     document.getElementById("onthoud4").classList.toggle("is-active"); 
-// }
-// function onthoud5(){
-//     if(canThrow[4] == false){
-//         canThrow[4] = true;
-//     }else{canThrow[4] = false;}
-//     document.getElementById("onthoud5").classList.toggle("is-active"); 
-// }
+function add1(){
+    let name1 = document.getElementById("naam1");
+    let naam1 = name1.value;
+    document.getElementById("P1").innerHTML = naam1;
+    document.getElementById("naam1").remove();
+    document.getElementById("add1").remove();
+}
+function add2(){
+    let name2 = document.getElementById("naam2");
+    let naam2 = name2.value;
+    document.getElementById("P2").innerHTML = naam2;
+    document.getElementById("naam2").remove();
+    document.getElementById("add2").remove();
+}
